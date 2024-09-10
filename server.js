01ea8d62d3ee9f657b64d7625559230a2a089a72
krishnamouli8 <moulimorampudi@gmail.com>
2024-09-10 11:57:59 +0530

    commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
+
+let latestStatus = null;
+
+app.post('/api', (req, res) => {
+  const { statusCode } = req.body;
+  latestStatus = statusCode;
+  console.log('Received status:', statusCode);
+  res.sendStatus(200);
+});
+
+app.get('/api/status', (req, res) => {
+  res.json({ statusCode: latestStatus });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
